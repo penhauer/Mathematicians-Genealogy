7c9a0f509247635ad83685b19e9754f12ed72bb9
amirmohammad ghasemi <amirmohammad.ghas3mi@gmail.com>
2022-06-01 15:41:36 +0430

    Employ multiple processes

--- extract_ids.py
+++ extract_ids.py
@@ -6,12 +6,10 @@ from bs4 import BeautifulSoup
 import string
 import re
 import itertools
+from multiprocessing import Pool
 
-         
 
 link = 'https://www.genealogy.math.ndsu.nodak.edu/letter.php?letter={}'
-id_link = 'https://www.genealogy.math.ndsu.nodak.edu/id.php?id={}'
-
 
 
 def get_with_prefix(prefix):
@@ -26,8 +24,10 @@ def more_depth_is_needed(text):
 def get_id_from_link(link):
     return link.attrs["href"].split("id=")[1]
 
+
 def get_name_from_link(link):
     return link.string
+
 
 def get_info_from_row(row):
     print("\n\n\n\n\n\n")
@@ -56,46 +56,16 @@ def crawl(prefix):
         return extract_info_from_leaf_page(text)
 
 
-def get_page_with_id(mathmatician_id):
-    r = requests.get(id_link.format(mathmatician_id))
-    return r.text
-
-def extract_info_from_page(text):
-    soup = BeautifulSoup(text, 'html.parser')
-    def extract_name():
-        return soup.find_all("h2")[0].text.strip("\n").strip(" ")
-
-    def extract_year():
-        year_element = soup.find('div', attrs={'style':'line-height: 30px; text-align: center; margin-bottom: 1ex'})
-        year = year_element.find_all('span')[0].contents[-1].strip(" ")
-        return year
-
-    def extract_advisors():
-        paragraphs = soup.find_all("p")
-        advisors = [p for p in paragraphs if "Advisor" in p.text][0]
-        advisors = list(map(get_id_from_link, advisors.find_all("a")))
-        return advisors
+def crawl_and_write_to_file(prefix):
+    items = crawl(prefix)
+    f = open(f"{prefix}.txt", "a")
+    for item in items:
+        f.write("{}\t\t{}\n".format(item[0], item[1]))
+    f.close()
 
 
-    name = extract_name()
-    year = extract_year()
-    advisors = extract_advisors()
-    return name, year, advisors
 
-def page_exists(text):
-    return not "You have specified an ID that does not exist in the database" in text
-
-
-def check_pages():
-    for i in range(5):
-        text = get_page_with_id(i)
-        if page_exists(text):
-            extract_info_from_page(text)
-
-s = input()
-items = crawl(s)
-f = open("output.txt", "a")
-for item in items:
-    f.write("{}\t\t{}\n".format(item[0], item[1]))
-f.close()
-
+prefix = input()
+prefixes = list(map(lambda c: prefix + c, string.ascii_uppercase))
+pool = Pool(processes=6)
+pool.map(crawl_and_write_to_file, prefixes)
