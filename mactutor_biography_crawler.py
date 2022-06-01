7420b7352e0a72ea912df38ee9f2132bd5ee99ba
Seyyed Mohammad Sadegh Keshavarzi <mohammadsadeghkeshavarzi@yahoo.com>
2022-06-01 17:26:57 +0430

    Do not fetch duplicate links

--- mactutor_biography_crawler.py
+++ mactutor_biography_crawler.py
@@ -13,28 +13,32 @@ regex = re.compile(r'[^a-z\s]')
 punctuation = re.compile('[' + string.punctuation + ']')
 porter = PorterStemmer()
 
+previous_urls = []
+
 for line in sys.stdin:
     current_url = line.strip().split(';')[2].strip()
-    url = urllib.request.urlopen(current_url)
-    content = url.read()
-    soup = BeautifulSoup(content, 'lxml')
 
-    name = soup.main.div.text.strip().replace(' ', '_')
+    if current_url not in previous_urls:
+        url = urllib.request.urlopen(current_url)
+        content = url.read()
+        soup = BeautifulSoup(content, 'lxml')
 
-    if name:
+        name = soup.main.div.text.strip().replace(' ', '_')
 
-        text = soup.main.get_text().lower()
-        text = re.sub(punctuation, ' ', text)
-        text = re.sub(regex, '', text)
-        words = word_tokenize(text)
-        words = [word for word in words if len(word) > 1]
-        words = [word for word in words if not word.startswith('http')]
-        stop_words = stopwords.words('english')
-        words = [word for word in words if word not in stop_words]
-        stems = [porter.stem(word) for word in words]
+        if name:
+            previous_urls.append(current_url)
+            text = soup.main.get_text().lower()
+            text = re.sub(punctuation, ' ', text)
+            text = re.sub(regex, '', text)
+            words = word_tokenize(text)
+            words = [word for word in words if len(word) > 1]
+            words = [word for word in words if not word.startswith('http')]
+            stop_words = stopwords.words('english')
+            words = [word for word in words if word not in stop_words]
+            stems = [porter.stem(word) for word in words]
 
-        print(name)
-        file = open(f'texts/{name}', 'w+')
-        file.write(' '.join(stems))
-        file.close()
-        print()
+            print(name)
+            file = open(f'texts/{name}', 'w+')
+            file.write(' '.join(stems))
+            file.close()
+            print()
