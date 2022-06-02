8a695d7e4e21a6ba0bb06de115ea312ecf4ff852
Seyyed Mohammad Sadegh Keshavarzi <mohammadsadeghkeshavarzi@yahoo.com>
2022-06-02 20:41:22 +0430

    Add option for not preprocessing biographies

--- mactutor_biography_crawler.py
+++ mactutor_biography_crawler.py
@@ -13,6 +13,9 @@ regex = re.compile(r'[^a-z\s]')
 punctuation = re.compile('[' + string.punctuation + ']')
 porter = PorterStemmer()
 
+prep = input('Should I preprocess? y/n')
+prep = prep == 'y'
+
 for line in sys.stdin:
     current_url = line.strip().split(';')[2].strip()
     url = urllib.request.urlopen(current_url)
@@ -22,14 +25,15 @@ for line in sys.stdin:
     name = soup.main.div.text.strip().replace(' ', '_')
 
     if name:
-        text = soup.main.get_text().lower()
-        text = re.sub(punctuation, ' ', text)
-        text = re.sub(regex, '', text)
-        words = word_tokenize(text)
-        words = [word for word in words if len(word) > 1]
-        stop_words = stopwords.words('english')
-        words = [word for word in words if word not in stop_words]
-        stems = [porter.stem(word) for word in words]
+        if prep:
+            text = soup.main.get_text().lower()
+            text = re.sub(punctuation, ' ', text)
+            text = re.sub(regex, '', text)
+            words = word_tokenize(text)
+            words = [word for word in words if len(word) > 1]
+            stop_words = stopwords.words('english')
+            words = [word for word in words if word not in stop_words]
+            stems = [porter.stem(word) for word in words]
 
         print(name)
         file = open(f'texts/{name}', 'w+')
