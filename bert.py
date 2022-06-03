fb02c6a1dbfb33cc780945295b2cf6bc7e7c95b8
Seyyed Mohammad Sadegh Keshavarzi <mohammadsadeghkeshavarzi@yahoo.com>
2022-06-03 16:17:45 +0430

    Pickle BERT doc embeddings

--- bert.py
+++ bert.py
@@ -1,46 +1,55 @@
-from pprint import pprint
-from os import listdir
-from typing import Dict, List, Tuple
+import os
+import pickle
 
 import numpy as np
-from numpy import linalg as LA
 
+from typing import Dict, List, Tuple
 from nltk import sent_tokenize
+from numpy import linalg as LA
 from sentence_transformers import SentenceTransformer
 
-MODEL_FILENAME = 'fasttext_model.bin'
 VECTOR_SIZE = 384
+EMBEDDINGS_FILENAME = 'embeddings.pkl'
 
 
 def get_biography_vector(filename: str, model: SentenceTransformer) -> np.ndarray:
-    res = np.array([0] * VECTOR_SIZE).astype('float64')
+    print(filename)
+    res = np.zeros(VECTOR_SIZE)
     length = 0
-    with open(f'./short/{filename}', 'r') as f:
+    with open(f'./raw/{filename}', 'r') as f:
         for line in f:
-            for sent in sent_tokenize(line):
+            sentences = sent_tokenize(line)
+            length += len(sentences)
+            for sent in sentences:
                 vector = model.encode([sent])[0]
-                length += 1
                 res += vector
-    return res / length
+    res /= length
+    res /= LA.norm(res)
+    return res
 
 
-def get_biographies_vector(model: SentenceTransformer) -> Dict[str, np.ndarray]:
+def get_biographies_vectors(model: SentenceTransformer) -> Dict[str, np.ndarray]:
+    if os.path.isfile(EMBEDDINGS_FILENAME):
+        with open(EMBEDDINGS_FILENAME, 'rb') as f:
+            return pickle.load(f)
     res = dict()
-    filenames = listdir('./short/')
+    filenames = os.listdir('./raw/')
     for filename in filenames:
         v = get_biography_vector(filename=filename, model=model)
         res[filename] = v
+    with open(EMBEDDINGS_FILENAME, 'wb+') as f:
+        pickle.dump(res, f, protocol=pickle.HIGHEST_PROTOCOL)
     return res
 
 
 def get_similarity(v1: np.ndarray, v2: np.ndarray):
-    return np.dot(v1, v2) / (LA.norm(v1) * LA.norm(v2))
+    return np.dot(v1, v2)
 
 
 def get_similarities(word: np.ndarray, vectors: Dict[str, np.ndarray]) -> List[Tuple[float, str]]:
     res = []
     for k, v in vectors.items():
-        s = get_similarity(word, v)
+        s = float(get_similarity(word, v))
         res.append((s, k))
     return res
 
@@ -53,10 +62,14 @@ def get_most_relevant_mathematicians(similarities: List[Tuple[float, str]], numb
 
 if __name__ == '__main__':
     model = SentenceTransformer('all-MiniLM-L6-v2')
+    biography_vectors = get_biographies_vectors(model=model)
     INPUT = input('Enter your term: ')
-    word_vector = model.encode([INPUT])[0]
-    biography_vectors = get_biographies_vector(model=model)
+    while INPUT != 'exit':
+        word_vector = model.encode([INPUT])[0]
+        word_vector /= LA.norm(word_vector)
 
-    similarities = get_similarities(word_vector, biography_vectors)
-    pprint(get_most_relevant_mathematicians(similarities, 4))
-    print('end')
+        similarities = get_similarities(word_vector, biography_vectors)
+        ans = get_most_relevant_mathematicians(similarities, 10)
+        ans = [name.replace('_', ' ') for name in ans]
+        print(*ans, sep='\n')
+        INPUT = input('Enter your term: ')
