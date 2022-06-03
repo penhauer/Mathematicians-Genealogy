5b0342106ec3d864fefceafa4358adef60df007e
Seyyed Mohammad Sadegh Keshavarzi <mohammadsadeghkeshavarzi@yahoo.com>
2022-06-03 16:38:57 +0430

    Refactor Boolean retrieval

--- boolean.py
+++ boolean.py
@@ -4,14 +4,26 @@ import glob
 
 import pandas as pd
 
-from tqdm import tqdm
 from nltk.stem.porter import PorterStemmer
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 
-regex = re.compile(r'[^a-z\s]')
 punctuation = re.compile('[' + string.punctuation + ']')
-porter = PorterStemmer()
+nonstandard = re.compile(r'[^a-z\s]')
+stop_words = stopwords.words('english')
+stemmer = PorterStemmer()
+
+
+def preprocess(text: str):
+    text = text.lower()
+    text = re.sub(punctuation, ' ', text)
+    text = re.sub(nonstandard, '', text)
+    text = word_tokenize(text)
+    text = [token for token in text if len(token) > 1]
+    text = [token for token in text if token not in stop_words]
+    text = [stemmer.stem(token) for token in text]
+    return text
+
 
 unique_words = set()
 
@@ -36,28 +48,22 @@ for file in glob.glob('short/*'):
             doc[word] = 0
 
     docs.append(doc)
-    unique_words = unique_words.union(words)
 
 columns = ['DOCUMENT_NAME'] + list(unique_words)
 docs = pd.DataFrame(docs, columns=columns)
-stop_words = stopwords.words('english')
 
 k = 10
 
 query = input('What are you looking for? ')
 while query != 'exit':
-    query = query.lower()
-    query = re.sub(punctuation, ' ', query)
-    query = re.sub(regex, '', query)
-    query = word_tokenize(query)
-    query = [word for word in query if len(word) > 1]
-    query = [word for word in query if word not in stop_words]
-    query = [porter.stem(word) for word in query]
+    query = preprocess(query)
 
     ans = docs.copy()
+
     for word in query:
         if len(ans) > 0:
             ans = ans[ans[word] == 1]
+
     ans = list(ans.head(k)['DOCUMENT_NAME'])
     ans = [word.replace('_', ' ') for word in ans]
     print(*ans, sep='\n')
