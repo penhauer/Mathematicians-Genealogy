e1a6eaa4a3a24c6142aa489a5c7e98e3f7a4975a
Seyyed Mohammad Sadegh Keshavarzi <mohammadsadeghkeshavarzi@yahoo.com>
2022-06-03 16:49:48 +0430

    Refactor TFIDF

--- TFIDF.py
+++ TFIDF.py
@@ -8,18 +8,30 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk import word_tokenize
 from sklearn.metrics.pairwise import cosine_similarity
 
-regex = re.compile(r'[^a-z\s]')
+nonstandard = re.compile(r'[^a-z\s]')
 punctuation = re.compile('[' + string.punctuation + ']')
-porter = PorterStemmer()
 stop_words = stopwords.words('english')
+stemmer = PorterStemmer()
+
+
+def preprocess(text: str) -> list[str]:
+    text = text.lower()
+    text = re.sub(punctuation, ' ', text)
+    text = re.sub(nonstandard, '', text)
+    text = word_tokenize(text)
+    text = [token for token in text if len(token) > 1]
+    text = [token for token in text if token not in stop_words]
+    text = [stemmer.stem(token) for token in text]
+    return text
+
 
 filenames = [filename for filename in glob.glob('short/*')]
 
 files = []
-for f in filenames:
-    f = open(f, 'r')
-    files.append(f.read())
-    f.close()
+for filename in filenames:
+    file = open(filename, 'r')
+    files.append(file.read())
+    file.close()
 
 vectorizer = TfidfVectorizer(strip_accents='unicode', stop_words='english')
 matrix = vectorizer.fit_transform(files)
@@ -30,16 +42,11 @@ filenames = [filename[6:] for filename in filenames]
 
 query = input('What are you looking for? ')
 while query != 'exit':
-    query = query.lower()
-    query = re.sub(punctuation, ' ', query)
-    query = re.sub(regex, '', query)
-    query = word_tokenize(query)
-    query = [word for word in query if len(word) > 1]
-    query = [word for word in query if word not in stop_words]
-    query = [porter.stem(word) for word in query]
+    query = preprocess(query)
     query = ' '.join(query)
-    query_vec = vectorizer.transform([query])
-    results = reversed(cosine_similarity(matrix, query_vec).reshape((-1)).argsort()[-k:])
+    query = vectorizer.transform([query])
+
+    results = reversed(cosine_similarity(matrix, query).reshape((-1)).argsort()[-k:])
     for i in results:
         print(filenames[i].replace('_', ' '))
     query = input('What are you looking for? ')
