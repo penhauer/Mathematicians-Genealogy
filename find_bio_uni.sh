fd7daea1458f34d7315afefc7bbf04799e33cec3
Seyyed Mohammad Sadegh Keshavarzi <mohammadsadeghkeshavarzi@yahoo.com>
2022-06-12 22:09:08 +0430

    Exactly match names when finding university

--- find_bio_uni.sh
+++ find_bio_uni.sh
@@ -4,7 +4,7 @@ while read -r line
 do
 	name=$(echo ${line} | awk -F';' '{print $1}')
 	name=$(echo -e "${name}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-	uni=$(ggrep -m 1 -P "\t${name}\t" names-and-ids/sorted.txt)
+	uni=$(ggrep -m 1 -P "\t\t${name}\t\t" names-and-ids/sorted.txt)
 	if [ -n "$uni" ]; then
 		echo "${uni}"
 	fi
