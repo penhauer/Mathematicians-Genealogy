85d5640595d6ba40d3a1acef5225a434a4a028e4
Seyyed Mohammad Sadegh Keshavarzi <mohammadsadeghkeshavarzi@yahoo.com>
2022-06-12 21:40:46 +0430

    Extract the universities of people in MacTutor

---
+++ find_bio_uni.sh
@@ -0,0 +1,11 @@
+input="list.txt"
+
+while read -r line
+do
+	name=$(echo ${line} | awk -F';' '{print $1}')
+	name=$(echo -e "${name}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+	uni=$(ggrep -m 1 -P "\t${name}\t" names-and-ids/sorted.txt)
+	if [ -n "$uni" ]; then
+		echo ${uni}
+	fi
+done < ${input}
