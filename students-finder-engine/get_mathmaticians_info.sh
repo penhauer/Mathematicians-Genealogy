53b493ab3f0dce34a70a68a98e1d24e79a336d3b
amirmohammad ghasemi <amirmohammad.ghas3mi@gmail.com>
2022-06-13 08:48:52 +0430

    Move files to related dir.

--- students-finder-engine/get_mathmaticians_info.sh
+++ students-finder-engine/get_mathmaticians_info.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 ./extract_ids
+mkdir names-and-ids
 cat names-and-ids/* | sort --numeric-sort | uniq > all_mathematicians.txt
 cat all_mathematicians.txt | awk '{print $1}' | sort --numeric-sort --check
 if [[ $? == 1 ]]; then
