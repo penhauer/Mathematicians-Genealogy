518cdaef026f8b8e40112cdb0860d1516237b08d
amirmohammad ghasemi <amirmohammad.ghas3mi@gmail.com>
2022-06-26 19:40:44 +0430

    Add HITS

--- ranking/ranking.py
+++ ranking/ranking.py
@@ -1,21 +1,15 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+plt.rcParams["figure.figsize"] = (40, 22)
 
 def do_ranking(mat):
-    # mat = np.matrix(
-    #     [
-    #         [1, 0, 0, 0],
-    #         [1, 0, 0, 0],
-    #         [1, 0, 1, 0],
-    #         [1, 0, 0, 0],
-    #     ]
-    # )
-
     G = nx.from_numpy_matrix(mat)
-
     pr = nx.pagerank(G, alpha=0.9)
+    hub, authority = nx.hits(G)
+
     print(pr)
+    print(hub, authority)
 
     nx.draw_networkx(G)
     plt.show()
